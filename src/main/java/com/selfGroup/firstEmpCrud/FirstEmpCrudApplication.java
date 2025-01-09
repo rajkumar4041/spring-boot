6644d3e38a77ced94dac72d722bb762a02628412
Rajkumar rathod <rajkumarrathod414@gmail.com>
2025-01-09 23:47:36 +0530

    initial commit

---
+++ src/main/java/com/selfGroup/firstEmpCrud/FirstEmpCrudApplication.java
@@ -0,0 +1,14 @@
+package com.selfGroup.firstEmpCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstEmpCrudApplication {
+
+	public static void main(String[] args) {
+	System.out.println("hello world !");
+		SpringApplication.run(FirstEmpCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfGroup/firstEmpCrud/FirstEmpCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfGroup.firstEmpCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstEmpCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
