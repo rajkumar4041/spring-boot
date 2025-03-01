3e7b5f7b5b61b815feda46c2a236f024af0d7ff5
Rajkumar rathod <127109370+rajkumar4041@users.noreply.github.com>
2025-03-02 01:49:57 +0530

    feat: dynamic file storing (#22)

--- src/main/java/com/selfGroup/firstEmpCrud/FileUploadHelper.java
+++ src/main/java/com/selfGroup/firstEmpCrud/FileUploadHelper.java
@@ -1,8 +1,10 @@
 package com.selfGroup.firstEmpCrud;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,7 +19,13 @@ import java.nio.file.StandardCopyOption;
 public class FileUploadHelper {
 
     //    public final String FILE_DIRECTORY = "/assets/uploadedFiles/";
-    public final String FILE_DIRECTORY = "/home/ttpl-lnv-0255/Desktop/spring-boot/firstEmpCrud/src/main/assets/uploadedFiles/";
+//    public final String FILE_DIRECTORY = "/home/ttpl-lnv-0255/Desktop/spring-boot/firstEmpCrud/src/main/assets/uploadedFiles/";
+    //dynamic path
+    public final String FILE_DIRECTORY = new ClassPathResource("/assets/uploadedFiles/").getFile().getAbsolutePath() + "/";
+
+    public FileUploadHelper() throws IOException {
+    }
+
 
     public ResponseEntity<String> validateUploadFile(MultipartFile file) throws IOException {
 
@@ -39,7 +47,18 @@ public class FileUploadHelper {
 //         Files.write(filePath, file.getBytes());
 
 
-        return ResponseEntity.ok("SuccessFully Uploaded File");
+//        response with path
+//        return ResponseEntity.ok("SuccessFully Uploaded File");
+//                                                                   if path then website-path/...
+//           ServletUriComponentsBuilder.fromCurrentContextPath() -> localHost://9000/<path>/<filename>
+        return ResponseEntity.ok(
+                ServletUriComponentsBuilder.fromCurrentContextPath()
+                        .path("/uploadedFiles/")
+                        .path(file.getOriginalFilename())
+                        .toUriString()
+        );
+
+//        return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/uploadedFiles/").path(file.getOriginalFilename()).toUriString());
 
     }
 
